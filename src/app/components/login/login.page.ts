9792da5ad7620b290d8f72523fbcd27e4774f2e3
Luis Lopez <gestor.jornada.app@gmail.com>
2019-03-02 21:26:42 -0600

    [Agragado] Servicio de autenticación con Firebase

---
+++ src/app/components/login/login.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService} from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  email: string;
+  password: string;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  onSubmitLogin() {
+    this.authService.login(this.email, this.password);
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,14 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBZ3CnmTkF0dk_wYm4Wwo2vTe6hXRTs370',
+  authDomain: 'organizador-de-jornadas.firebaseapp.com',
+  databaseURL: 'https://organizador-de-jornadas.firebaseio.com',
+  projectId: 'organizador-de-jornadas',
+  storageBucket: 'organizador-de-jornadas.appspot.com',
+  messagingSenderId: '460341045396'
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
